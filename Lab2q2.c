90d546130655edcde83bcb9fe197fa0937797f06
singhanushka0214 <97422676+singhanushka0214@users.noreply.github.com>
2023-08-20 12:24:33 +0530

    Create Lab2q2.c

---
+++ Lab2q2.c
@@ -0,0 +1,15 @@
+//Bit stuffing
+#include<stdio.h>
+int main()
+{
+    unsigned int x = 0x11223344;
+    char *value = (char *) &x;
+    if(*value == 0x11)
+    {
+        printf("Big Endian %02X\n", *value);
+    }
+    else
+    {
+        printf("Little Endian %02X\n", *value);
+    }
+}
